5fc555fa709a32536317eec0281c830095a17ba0
thinhpham2k2 <thinhphamquoc9999@gmail.com>
2024-12-04 08:38:14 +0700

    Init project

---
+++ core-service/src/test/java/com/assignment/core_service/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
